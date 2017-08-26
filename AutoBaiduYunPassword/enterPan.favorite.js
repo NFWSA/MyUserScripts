2ade645f680191be405d690b54ebc9b6c4d16da0
SurgeNight <surgenight@hotmail.com>
2017-08-27 01:32:22 +0800

    Update AutoBaiduYunPwd's enterPan, make it could work as normal with some incompleted string (especially the shared link).

--- AutoBaiduYunPassword/enterPan.favorite.js
+++ AutoBaiduYunPassword/enterPan.favorite.js
@@ -8,6 +8,25 @@ function(){
     pwd = pwd.split('：');
     pwd = pwd[pwd.length - 1];
     text = text[0];
+    text = text.split(' ');
+    if (text.length > 1)
+        text = text[text.length - 1];
+    else
+        text = text[0];
+    text = text.split(':');
+    if (text.length > 1)
+        text = text[text.length - 1];
+    else
+        text = text[0];
+    text = text.split('：');
+    if (text.length > 1)
+        text = text[text.length - 1];
+    else
+        text = text[0];
+    if (!text.includes("/"))
+        text = "https://pan.baidu.com/s/" + text;
+    else
+        text = "https:" + text;
     window.open(text, 'AutoPwdPan=' + pwd);
 }
 )();
