b939dc9f82a696c311bda13412f9ff9902f22df7
SurgeNight <surgenight@hotmail.com>
2018-04-16 16:57:22 +0800

    add the script that hide the zhihu.com's sidebar.

---
+++ HideZhiHuSidebar/main.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Hide ZhiHu Sidebar
+// @namespace    SN_HideZhiHuAd
+// @version      0.1
+// @description  hide the zhihu.com's right sidebar.
+// @author       SurgeNight
+// @match        http://www.zhihu.com/question/*
+// @grant        none
+// @run-at document-idle
+// ==/UserScript==
+
+(function() {
+	document.getElementsByClassName("Sticky is-fixed")[3].hidden = true;
+	setInterval(() => {
+		document.getElementsByClassName("Sticky is-fixed")[0].className = "Sticky AppHeader is-fixed";
+		document.getElementsByClassName("Sticky is-fixed")[0].getElementsByClassName("PageHeader")[0].className = "PageHeader";
+	}, 200);
+})();
+
+
