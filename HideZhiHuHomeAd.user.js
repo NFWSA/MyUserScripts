07e31ae7be87633249139e3cb615e230df899fbe
SurgeNight <surgenight@hotmail.com>
2018-11-02 10:58:08 +0800

    optimize the code that hide ad.

--- HideZhiHuHomeAd.user.js
+++ HideZhiHuHomeAd.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hide ZhiHu Homepage Ad
 // @namespace    SN_HideZhiHuHomeAd
-// @version      0.1
+// @version      0.11
 // @description  at zhihu.com's homepage, hide the ads in right sidebar and list container.
 // @author       SurgeNight
 // @match        http*://www.zhihu.com
@@ -11,7 +11,7 @@
 
 (function() {
 	for (var x of document.getElementsByClassName("Card Banner"))
-		x.hidden = true;
+		x.remove();
 	for (var x of document.getElementsByClassName("TopstoryItem--advertCard"))
-		x.hidden = true;
+		x.remove();
 })();
