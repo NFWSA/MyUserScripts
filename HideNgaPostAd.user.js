1d5d653d5266df768b148ca72bad17672938b4c2
SurgeNight <surgenight@hotmail.com>
2021-11-05 11:45:29 +0800

    Add new usersciprt to hide ad of nga post.

---
+++ HideNgaPostAd.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hide NGA Post Ad
+// @namespace    SN_HideNGAPostAd
+// @version      0.1
+// @description  at bbs.nga.cn, hide the ads in post.
+// @author       SurgeNight
+// @match        https://bbs.nga.cn/read.php?tid=*
+// @grant        none
+// @run-at       document-idle
+// ==/UserScript==
+
+(function() {
+	let ele = document.createElement('style');
+	ele.innerHTML = "#m_posts_c > span { display: none; }";
+	document.head.insertBefore(ele, document.head.lastElementChild);
+})();
