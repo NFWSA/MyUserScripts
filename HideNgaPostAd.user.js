316e88e021c22cc9289a13e26c412e63912befba
SurgeNight <surgenight@hotmail.com>
2021-12-01 17:23:00 +0800

    Add more hide function in nga script.

--- HideNgaPostAd.user.js
+++ HideNgaPostAd.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Hide NGA Post Ad
 // @namespace    SN_HideNGAPostAd
-// @version      0.2
-// @description  at bbs.nga.cn, hide the ads in post.
+// @version      0.3
+// @description  at bbs.nga.cn, hide the ads in post, hide avatar and img except hover.
 // @author       SurgeNight
 // @match        https://bbs.nga.cn/read.php?tid=**
 // @match        https://ngabbs.com/read.php?tid=**
@@ -12,6 +12,11 @@
 
 (function() {
     let ele = document.createElement('style');
-    ele.innerHTML = "#m_posts_c > span { display: none; }";
+    ele.innerHTML = "#m_posts_c > span { display: none; }\n";
+    ele.innerHTML += "div#b_nav > div { display: none; }\n";
+    ele.innerHTML += "div#b_nav > div.nav { display: block; }\n";
+    ele.innerHTML += "span#fast_post_c > div > div > table > tbody > tr > td:nth-child(2) { display: none; }\n";
+    ele.innerHTML += "img.smile_ac, img.avatar { opacity: 0.05; }\n";
+    ele.innerHTML += "img.smile_ac:hover, img.avatar:hover { opacity: 1; }\n";
     document.head.insertBefore(ele, document.head.lastElementChild);
 })();
