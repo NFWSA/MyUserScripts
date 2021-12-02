37666916656999f1566cb1bc8daa7f2a13c4973e
SurgeNight <surgenight@hotmail.com>
2021-12-02 15:41:02 +0800

    Jump ad page in nga script.

--- HideNgaPostAd.user.js
+++ HideNgaPostAd.user.js
@@ -1,16 +1,21 @@
 // ==UserScript==
 // @name         Hide NGA Post Ad
 // @namespace    SN_HideNGAPostAd
-// @version      0.3
-// @description  at bbs.nga.cn, hide the ads in post, hide avatar and img except hover.
+// @version      0.4
+// @description  at bbs.nga.cn, hide and jump ads, hide avatar and img except hover.
 // @author       SurgeNight
 // @match        https://bbs.nga.cn/read.php?tid=**
 // @match        https://ngabbs.com/read.php?tid=**
+// @match        https://bbs.nga.cn/misc/adpage_insert_2.html**
 // @grant        none
 // @run-at       document-idle
 // ==/UserScript==
 
 (function() {
+    if (location.href.includes("/adpage_insert_2")) {
+        getJump();
+        return;
+    }
     let ele = document.createElement('style');
     ele.innerHTML = "#m_posts_c > span { display: none; }\n";
     ele.innerHTML += "div#b_nav > div { display: none; }\n";
